c550ecd9613dffa009bbc1502d1b43bb06270728
Daniel Pena <dakyom.dev@gmail.com>
2026-01-12 23:08:47 -0300

    fix sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
             allow: '/',
             disallow: '/private/',
         },
-        sitemap: 'https://citando-ando.vercel.app/sitemap.xml',
+        sitemap: 'https://citando-ando.vercel.app/sitemap.xml/',
     };
 }
